38ce24962fbad0d902f523054dba0f8100d4a6a7
green-dv <jip2027354@est.univalle.edu>
2025-06-22 08:44:59 -0400

    Commit inicial de la api del modelo CNN

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from ultralytics import YOLO
+from PIL import Image
+import io
+import json
+
+model = YOLO("model/best.pt")
+
+app = FastAPI()
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+
+    results = model(image)
+
+    raw_json = results[0].tojson()
+
+    return JSONResponse(content=json.loads(raw_json))
